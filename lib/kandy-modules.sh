48b5244408ccf3d7c9fcbff0aca5d6d9a48296f4
Amandeep Ghai <amandeep@keepworks.com>
2016-05-03 00:14:25 +0530

    Changes for kandy-redux local video visibility

--- lib/kandy-modules.sh
+++ lib/kandy-modules.sh
@@ -7,7 +7,7 @@ download_kandy_modules() {
   echo "Made the directory for kandy-modules"
   cd ${KANDY_MODULES_DIRECTORY}
   echo "Starting git clone of kandy-redux"
-  git clone https://${GITHUB_ACCESS_TOKEN}:x-oauth-basic@github.com/Fring/kandy-redux.git
+  git clone https://${GITHUB_ACCESS_TOKEN}:x-oauth-basic@github.com/Fring/kandy-redux.git -b localVideoInVideoCalls
   echo "Starting git clone of Kandy.JS "
   git clone https://${GITHUB_ACCESS_TOKEN}:x-oauth-basic@github.com/Fring/Kandy.js.git -b master
 }
